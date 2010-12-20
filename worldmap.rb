1a0f509de9fc8b4bb796373ef166784afc4e9061
Jacob Perkins <jacob.a.perkins@gmail.com>
2010-12-20 00:40:13 -0600

    worldmap

---
+++ worldmap.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+require 'swineherd'
+require 'swineherd/script/r_script' ; include Swineherd::Script
+
+Settings.define :width,  :default => 84,    :description => "Plot width in inches"
+Settings.define :height, :default => 48,    :description => "Plot height in inches"
+Settings.define :lat,    :default => 1,     :description => "Latitude field"
+Settings.define :lon,    :default => 2,     :description => "Longitude field"
+Settings.resolve!
+
+raise "No input data!" unless Settings.rest.first
+
+plotter = RScript.new('templates/worldmap.R.erb')
+plotter.output << File.basename(Settings.rest.first).gsub(File.extname(Settings.rest.first), ".pdf")
+plotter.attributes = {
+  :data      => Settings.rest.first,
+  :plot      => plotter.output,
+  :latitude  => Settings.lat,
+  :longitude => Settings.lon,
+  :width     => Settings.width,
+  :height    => Settings.height
+}
+plotter.run true
