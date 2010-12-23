c2ffd33841eaf38fda69341038e04c13eb624d5b
Jacob Perkins <jacob.a.perkins@gmail.com>
2010-12-23 08:16:42 -0600

    modified xyplot to use ggplot2 (it looks better) and gives the option to use line or point type

--- xyplot.rb
+++ xyplot.rb
@@ -3,11 +3,13 @@
 require 'swineherd'
 require 'swineherd/script/r_script' ; include Swineherd::Script
 
-Settings.define :width,  :default => 1200, :description => "Plot width in pixels"
-Settings.define :x,      :default => 1,    :description => "Column to use as x values"
-Settings.define :y,      :default => 2,    :description => "Column to use as y values"
-Settings.define :xlab,   :default => "X",  :description => "X label"
-Settings.define :ylab,   :default => "Y",  :description => "Y label"
+Settings.define :width,      :default => 1200,  :description => "Plot width in pixels"
+Settings.define :x,          :default => 1,     :description => "Column to use as x values"
+Settings.define :y,          :default => 2,     :description => "Column to use as y values"
+Settings.define :points,     :default => false, :description => "Plot defaults to line, use points instead"
+Settings.define :xlab,       :default => "X",   :description => "X label"
+Settings.define :ylab,       :default => "Y",   :description => "Y label"
+Settings.define :point_size, :default => 1,     :description => "Width of line in pixels or size of points in pixels"
 Settings.resolve!
 
 raise "No input data!" unless Settings.rest.first
@@ -15,12 +17,14 @@ raise "No input data!" unless Settings.rest.first
 plotter = RScript.new('templates/xyplot.R.erb')
 plotter.output << File.basename(Settings.rest.first).gsub(File.extname(Settings.rest.first), ".png")
 plotter.attributes = {
-  :data => Settings.rest.first,
-  :plot => plotter.output,
-  :x    => Settings.x,
-  :y    => Settings.y,
-  :xlab => Settings.xlab,
-  :ylab => Settings.ylab,
-  :width  => Settings.width
+  :data   => Settings.rest.first,
+  :plot   => plotter.output,
+  :x      => Settings.x,
+  :y      => Settings.y,
+  :xlab   => Settings.xlab,
+  :ylab   => Settings.ylab,
+  :width  => Settings.width,
+  :size   => Settings.point_size,
+  :points => Settings.points
 }
 plotter.run true
